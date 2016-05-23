8d3c7239fbdd1cbc45a18fe0299999ab4a76e9d4
Karina Hagiwara <kariina-h@hotmail.com>
2016-05-22 21:23:51 -0300

    ProjEngSoftII - versão 2
    *funcionalidades:
      - verificar se é ímpar ou par;
      - verificar se é negativo ou positivo;
      - menu de opções

--- projEngSoftII.cpp
+++ projEngSoftII.cpp
@@ -1,18 +1,44 @@
 #include <stdio.h>
-//apenas funcoes
+
 int main(){
+	int opcao=0;
 	int numero=0;
-	scanf("%d", &numero);
-	if(numero<0){
-		printf("é negativo");
-	}else{
-		printf("é positivo");
-	}	
-	scanf("%d", &numero);
-	if((numero%2)==0){
-		printf("é par");
-	}else{
-		printf("é ímpar");
-	}	
+	int sair=0;
+	while(sair!=1){
+		scanf("%d", &opcao);
+		switch(opcao){
+			case 1:
+				numero=0;
+				scanf("%d", &numero);
+				if(numero<0){
+					printf("é negativo\n");
+				}else{
+					printf("é positivo\n");
+				}
+				break;
+			case 2:
+				numero=0;
+				scanf("%d", &numero);
+				if((numero%2)==0){
+					printf("é par\n");
+				}else{
+					printf("é ímpar\n");
+				}
+				break;
+			case 3:
+				numero=0;
+				scanf("%d", &numero);
+				if(numero==0){
+					sair=0;
+					break;
+				}else{
+					sair=1;
+					break;
+				}
+			default:
+				printf("não existe essa opcao\n");
+				break;
+			}
+		}
 	return 0;
 }
