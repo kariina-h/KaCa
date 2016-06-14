749d7280fcde62fd5404c0ecd08c839cfce37a70
Karina Hagiwara <kariina-h@hotmail.com>
2016-06-13 23:36:09 -0300

    projEngSoftII - versão 5
    *defeitos concertados*

--- projEngSoftII.cpp
+++ projEngSoftII.cpp
@@ -1,11 +1,11 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-//printf funcionalidade numero pos ou neg
+
 int main(){
 	int opcao=0;
 	float numero=0;
-	int num=0;
+	float num=0;
+	int aux=0;
 	int sair=0;
 	while(sair!=1){
 		printf("Menu opções:\n");
@@ -14,36 +14,43 @@ int main(){
 		printf("3- Sair.\n");
 		printf("Digite o número da opção desejada.\n");
 		scanf("%d", &opcao);
-	//	system("pause");
-	//	system("cls");
 		switch(opcao){
 			case 1:
 				numero=0;
 				printf("Verificar se o número é positivo ou negativo.\n");
 				printf("Digite um número:\n");
 				scanf("%f", &numero);
-				if(numero<0){
-					printf("é negativo\n");
+				if(numero==0){
+						printf("é neutro\n");
 				}else{
-					printf("é positivo\n");
+					if(numero<0){
+						printf("é negativo\n");
+					}else{
+						printf("é positivo\n");
+					}
 				}
 				break;
 			case 2:
 				num=0;
 				printf("Verificar se o número é ímpar ou par.\n");
 				printf("Digite um número inteiro:\n");
-				scanf("%d", &num);
-				if((num%2)==0){
-					printf("é par\n");
+				scanf("%f", &num);
+				aux=num;
+				if (aux==num){
+					if((aux%2)==0){
+						printf("é par\n");
+					}else{
+						printf("é ímpar\n");
+					}
 				}else{
-					printf("é ímpar\n");
+					printf("Digite um número inteiro.\n");
 				}
 				break;
 			case 3:
 				num=0;
 				printf("Deseja realmente sair?\n");
 				printf("Digite 1 se deseja sair, caso contrário digite 0.\n");
-				scanf("%d", &num);
+				scanf("%f", &num);
 				if(num==0){
 					sair=0;
 					break;
@@ -52,7 +59,7 @@ int main(){
 					break;
 				}
 			default:
-				printf("não existe essa opcao\n");
+				printf("Não existe essa opcao\n");
 				break;
 			}
 			system("pause");
